3ce52bee1582ce97485c278786300e2a4b5170aa
La Van Bac <la.van.bac@sun-asterisk.com>
2019-11-21 14:04:57 +0700

    Update inject.js

--- js/inject.js
+++ js/inject.js
@@ -4,12 +4,9 @@ var answer = {
   answer_ids: [11725]
 };
 
-var timer = null;
-
 function auto_answer() {
   $.post(answer_url, answer).done(function(res) {
     if (!res.data.status) {
-      clearInterval(timer);
       $('#captcha-msg').html(
         'Extension bị lỗi, bạn tự trả lời nha ' +
         '<img src="https://s.yimg.com/lq/i/mesg/emoticons7/20.gif">'
@@ -17,13 +14,11 @@ function auto_answer() {
       return;
     }
 
-    let src = timer ? 'https://i.imgur.com/lpCFCsT.png' : 'https://i.imgur.com/w4t30W4.gif';
-
     $('#token-core-value').val(res.data.token);
     $('.capcha-wrap').html(
       '<p class="text-center" style="margin-top: 15px;">' +
       '<strong>Tự động trả lời thành công</strong> ' +
-      '<img width="70" src="' + src + '">' +
+      '<img width="70" src="https://i.imgur.com/w4t30W4.gif">' +
       '</p>'
     );
     $('.capcha-wrap').append(
@@ -32,7 +27,7 @@ function auto_answer() {
       '<img src="https://chatpp.thangtd.com/img/emoticons/giggle.gif"></i>'
     );
 
-    timer = setInterval(auto_answer, Settings.core_value.captcha_timeout);  
+    setTimeout(auto_answer, Settings.core_value.captcha_timeout);  
   });
 }
 
