a26f3cb5621de602467b61ad067b873d0fe74ba9
La Van Bac <la.van.bac@sun-asterisk.com>
2019-12-02 08:13:34 +0700

    remove indent

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
 const injectJsFile = file_name => {
-	let script = document.createElement('script');
-	script.src = chrome.extension.getURL(`js/${file_name}`);
-	document.documentElement.appendChild(script);
+  let script = document.createElement('script')
+  script.src = chrome.extension.getURL(`js/${file_name}`)
+  document.documentElement.appendChild(script)
 }
 
 const injectCssFile = file_name => {
-  let link = document.createElement('link');
-  link.rel = 'stylesheet';
-  link.type = 'text/css';
-  link.href = chrome.extension.getURL(`css/${file_name}`);
-  link.media = 'all';
-  document.head.appendChild(link);
+  let link = document.createElement('link')
+  link.rel = 'stylesheet'
+  link.type = 'text/css'
+  link.href = chrome.extension.getURL(`css/${file_name}`)
+  link.media = 'all'
+  document.head.appendChild(link)
 }
 
-injectCssFile('main.css');
-injectJsFile('inject.js');
+injectCssFile('main.css')
+injectJsFile('inject.js')
